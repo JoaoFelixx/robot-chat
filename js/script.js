af43a11f512bcdf8e61f1306dcdc6cb34ad88fa5
João Félix <joaosilva16387@gmail.com>
2021-03-13 14:05:05 -0300

    Segundo dia de trabalho

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 async function connectionWithRobot(database)  {
-  let acess  = await fetch(database);
-  let result = await acess.json();
-    return result
+  const acess  = await fetch(database);
+  const result = await acess.json();
+    return await result
 }
 
-async function start() {
-  const database = '../database/robot.json'
-  const Robot = await connectionWithRobot(database);
-  console.log(Robot)
+async function startRobot(database) {
+  return await connectionWithRobot(database);
 }
 
 
+const robot = startRobot('../database/Robot_req.json')
+  .then(resolve => {
+    
+    const question = prompt('Digite algo');
 
-window.onload = start;
+    console.log(resolve[question]);
+
+
+  })
+  .catch(error =>  console.warn('failed connection',error))
